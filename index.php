0762719c08e6f791f484b5fd1cede2810bcf21d2
andresgprojas <andresprojasg@gmail.com>
2014-05-02 19:13:11 -0500

    aplicar Bootstrap

--- index.php
+++ index.php
@@ -4,15 +4,43 @@
         <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
         <script src="http://malsup.github.com/jquery.form.js"></script> 
+        <link href="View/css/bootstrap.min.css" rel="stylesheet" media="screen">
         <style>
-            .campo label {
-                display: inline-block;
-                width: 10%;
-            }
-            body {
-                margin: 2% 15%;
-            }
-        </style>
+        .boxlogin{
+            margin: 50px auto;
+            width: 320px;
+            box-shadow: 0px 2px 10px #d6d6d6;
+            border-radius: 4px;
+            -webkit-border-radius: 4px;
+            -moz-border-radius: 10px;
+        }
+        #psw{
+            border-top-left-radius: 0px !important;
+            border-top-right-radius: 0px !important;
+        }
+        #nuip{
+            border-bottom-left-radius: 0px !important;
+            border-bottom-right-radius: 0px !important;
+        }
+        input[type="password"]{
+            margin-top: -1px;
+        }
+        input[type="submit"]{
+            margin-top: 15px;
+        }
+        input::-webkit-input-placeholder:before{
+            font-weight: bold;
+        }
+        input::-webkit-input-placeholder{
+            font-style: italic;
+        }
+        input:-moz-placeholder:before {
+            font-weight: bold;
+        }
+        input:-moz-placeholder {
+            font-style: italic;
+        }
+    </style>
         <script>
             $(function(){
                 options = {
@@ -31,19 +59,19 @@
         <title>Login</title>
     </head>
     <body>
-        <form id="login" method="POST" action="Controller/usuario" style="text-align: center">
-        
+        <div class="jumbotron boxlogin">
+        <form id="login" method="POST" action="Controller/usuario" class="form-signin" autocomplete="off">
+            <h2 class="form-signin-heading">Ingresa</h2>
             <div class="campo">
-                <label for="nuip">Usuario</label>
-                <input type="text" name="nuip" id="nuip" required placeholder="Identificación">
+                <input type="text" name="nuip" id="nuip" class="form-control" required placeholder="Identificación">
             </div>
             <div class="campo">
-                <label for="nuip">Contraseña</label>
-                <input type="password" name="psw" id="psw" required placeholder="Clave Secreta">
+                <input type="password" name="psw" id="psw" class="form-control" required placeholder="Contraseña">
             </div>
             <div class="campo">
-                <input type="submit" value="Ingresar">
+                <input type="submit" value="Acceder" class="btn btn-lg btn-primary btn-block">
             </div>
         </form>
+        </div>
     </body>
 </html>
